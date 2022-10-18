421dee016cd4e20eea808edddfaf8b385e23688e
해빈 <112257466+h-beeen@users.noreply.github.com>
2022-10-18 11:40:24 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[10];
+bool isused[10];
+
+void func(int k)
+{
+	if(k == m)
+	{
+		for(int i = 0; i < m; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	int st = 1;
+	if(k != 0) st = arr[k-1];
+	for(int i = st; i <= n; i++)
+	{
+		arr[k] = i;
+		func(k+1);
+	} 
+}
+
+int	main (void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n >> m;
+	func(0);
+}
