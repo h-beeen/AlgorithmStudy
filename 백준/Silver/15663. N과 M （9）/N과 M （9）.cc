358e005217188b66273c138d7399d990d8d4cbb0
해빈 <112257466+h-beeen@users.noreply.github.com>
2022-10-18 15:04:02 +0900

    [Silver II] Title: N과 M (9), Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -6,7 +6,7 @@ int arr[10];
 int num[10];
 bool isused[10];
 
-void func(int k) { // 현재 k개까지 수를 택했음.
+void func(int k) { 
   if (k == m) {
     for (int i = 0; i < m; ++i) {
       cout << arr[i] << ' ';
@@ -14,9 +14,9 @@ void func(int k) { // 현재 k개까지 수를 택했음.
     cout << '\n';
     return;
   }
-  int tmp = 0;  // 중복 수열인지 확인 하기 위해 필요한 임시 변수
+  int tmp = 0;
   for (int i = 0; i < n; ++i) {
-    if (!isused[i] && tmp != num[i]){ // 이전 수열의 마지막 항과 새로운 수열의 마지막 항이 같으면 중복 수열
+    if (!isused[i] && tmp != num[i]){
       isused[i] = true;
       arr[k] = num[i];
       tmp = arr[k];
