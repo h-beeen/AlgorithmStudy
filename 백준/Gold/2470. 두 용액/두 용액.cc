997405a959d90bd575d62f2c1d84f54129529b04
Haebin <joker7011@kyonggi.ac.kr>
2022-11-16 00:59:29 +0900

    [Gold V] Title: 두 용액, Time: 72 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n, sum_value, min_value, st, en;
+vector <int> res(2);
+
+int main(void)
+{
+    cin >> n;
+    vector <int> arr(n);
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    st = 0;                     // 투 포인터 start index
+    en = n - 1;                 // 투 포인터 end index 
+    min_value = 2147483647;
+
+    sort(arr.begin(), arr.end());
+
+    while(st < en)
+    {
+        sum_value = arr[st] + arr[en];
+        if(min_value > abs(sum_value))
+        {
+            min_value = abs(sum_value);
+            res[0] = arr[st];
+            res[1] = arr[en];
+        }
+
+        if(sum_value == 0)
+            break;
+
+        if(sum_value < 0)
+            st++;
+        else
+            en--;
+    }
+    cout << res[0] << ' ' << res[1];
+}
