7f678f1a55147bb37eeedf0061cfef670f2071d0
Haebin <joker7011@kyonggi.ac.kr>
2023-05-03 09:28:56 +0900

    [unrated] Title: 특수문자 출력하기, Time: 134.62 ms, Memory: 64.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = "!@#$%^&*(\\'\"<>?:;";
+        System.out.println(a);
+    }
+}
